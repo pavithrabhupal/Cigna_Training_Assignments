f1f1bf5abd38ca8f1be18c723ff36f75fe2fdd23
pavithrabhupal <pavithrabhupal2004@gmail.com>
2025-10-14 19:05:39 +0530

    Update SQL_Assignment-2.sql

--- SQL_Assignment-2.sql
+++ SQL_Assignment-2.sql
@@ -40,7 +40,42 @@ CREATE TABLE Order_Details (
   CONSTRAINT FK_product FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
 );
 
---Insert
+--Insert into product
+INSERT INTO Product(Product_ID, Product_Name, Category, Price, Stock) VALUES
+(101, 'Laptop', 'Electronic',123897.03, 10),
+(305, 'Headphone','Accessory', 899, 20);
+
+-- Inserting customers
+INSERT INTO Customers (Customer_ID, First_Name, Last_Name, E_mail, Phone) VALUES
+(1, 'Bob', 'Stew', 'bob@example.com', '+918675544329'),
+(2, 'Alia', 'Mark', 'alia@gmail.com', '+918234545674');
+
+-- Inserting orders
+INSERT INTO Orders (Order_ID, Customer_ID, Total_Amount) VALUES
+(1098, 1, TO_DATE('2025-01-10','YYYY-MM-DD'), 5000),
+(2876, 2, TO_DATE('2025-01-09','YYYY-MM-DD'), 20000);
+
+-- Inserting order details
+INSERT INTO Order_Details (ODeatil_ID, Quantity, Order_ID, Product_ID) VALUES
+(1, 1, 1, 50),
+(2, 2, 2, 100);
+
+SELECT * 
+FROM Products 
+WHERE Stock <20;
+
+SELECT SUM(O.order_amt)
+FROM Customers C
+INNER JOIN Orders O ON C.customer_id = O.customer_id
+GROUP BY C.customer_name;
+
+UPDATE Products P
+SET P.Stock = P.Stock - (
+    SELECT OD.qty
+    FROM OrderDetails OD
+    WHERE OD.product_id = P.product_id
+)
+WHERE P.product_id IN (SELECT DISTINCT product_id FROM OrderDetails);
 
 
 
